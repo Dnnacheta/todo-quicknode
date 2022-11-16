bcc711c1f7606e1e5db468b57ded49317014808a
David Nnacheta <68163484+Dnnacheta@users.noreply.github.com>
2022-11-16 14:56:09 +0100

    Update copy.deploy.js

--- copy.deploy.js
+++ copy.deploy.js
@@ -1,12 +1,10 @@
 async function main() {
   // Grab the contract factory 
-  const NFT = await ethers.getContractFactory("TodoApp"); // W3bNFT
+  const Todo = await ethers.getContractFactory("TodoApp"); // Todo
 
   console.log("Deployment started🔹🔹🔹, returning a promise that resolves to a contract object 🤞"); 
-
-  // const root = '0x8890fdaaf3cdfcde0537857464fe9faac18a6d185dc1992ca2f2b809057def57';
-  const wNFT = await NFT.deploy();  // 0x21fE7704f9Bb608d1b8673c3Fe78CFFC1B1C804a, 0xb04FF5C147405e883C47BE596dedA7682934c247
-  console.log("🎉Contract deployed to address🎉", wNFT.address);
+  const todo = await Todo.deploy(); 
+  console.log("🎉Contract deployed to address🎉", todo.address);
 }
 
 main()
